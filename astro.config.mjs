185da88b9a55c4463e36fac19c68e75cbe39b128
SHI66ie <148475282+SHI66ie@users.noreply.github.com>
2026-05-18 23:15:40 +0100

    fix: add @astrojs/netlify adapter + hybrid output to support API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://shadesworld.ng',
   integrations: [tailwind()],
-  output: 'static',
+  output: 'hybrid',
+  adapter: netlify(),
 });
